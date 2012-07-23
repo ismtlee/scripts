44ea954495b3d9e45d87866b326b823a0d2e77b6
Smart Lee <ismtlee@gmail.com>
2012-07-23 10:37:52 +0800

    .

--- install/nginx/install.sh
+++ install/nginx/install.sh
@@ -20,9 +20,9 @@ install() {
 	cd $download/nginx-$version
 	./configure --prefix=${prefix}/nginx --user=www --group=www  --with-http_stub_status_module 
 	make;make install
-	cp nginx /etc/init.d/
-	cp nginx.conf $prefix/nginx/conf
-	cp sites.conf $prefix/nginx/conf
+	cp $root/nginx /etc/init.d/
+	cp $root/nginx.conf $prefix/nginx/conf
+	cp $root/sites.conf $prefix/nginx/conf
 	chmod +x /etc/init.d/nginx
 	mkdir -p /logs/nginx
 	chown -R www /logs
