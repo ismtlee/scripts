99f85c28948d5105932f5f5d7480d8460b821329
Smart Lee <ismtlee@gmail.com>
2012-07-23 11:33:34 +0800

    .

--- install/nginx/first.sh
+++ install/nginx/first.sh
@@ -2,5 +2,20 @@
 # WARNING: Please excute this script only for first installation!
 source ../header.sh
 
+echo "The script only for first installation. This may cover your configuration."
+echo "Do you wish to proceed <y or n> ? \c"
+read WISH
+
+if [ $WISH = "n" ];then
+	echo bye
+	exit
+fi
+
+if [ $WISH != "y" ];then
+	echo "invalid option, bye"
+	exit
+fi
+
+
 cp $root/nginx.conf.init $prefix/nginx/conf/nginx.conf
 cp $root/sites.conf.init $prefix/nginx/conf/sites.conf
