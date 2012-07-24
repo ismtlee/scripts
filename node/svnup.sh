74f2c67172b31d08e393fb96ad950747f766643f
Smart Lee <ismtlee@gmail.com>
2012-07-24 16:07:01 +0800

    .

--- node/svnup.sh
+++ node/svnup.sh
@@ -1,2 +1,7 @@
-/usr/bin/svn cleanup /usr/deploy/orange_dev
-/usr/bin/svn up /usr/deploy/orange_dev
+#!/bin/sh
+update() {
+ /usr/bin/svn cleanup /usr/deploy$1
+ /usr/bin/svn up /usr/deploy$1
+}
+
+update $1
