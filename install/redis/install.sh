b9dfe275952db6cfd2dca3c423422aca877fe66b
Smart Lee <ismtlee@gmail.com>
2012-11-29 14:23:59 +0800

    .

---
+++ install/redis/install.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+source ../header.sh
+version=2.6.6
+
+dependencies() {
+}
+
+download() {
+  redis_tgz=redis-$version.tar.gz
+
+	if [ ! -f $download/$redis_tgz ];
+	then
+		wget http://redis.googlecode.com/files/$redis_tgz
+		tar zxvf $redis_tgz -C $prefix
+	fi
+}
+
+install() {
+	cd $prefix/redis-$version
+	make;make install
+}
+
+usergroup() {
+}
+
+config() {
+	cp $root/redis_6379 /etc/init.d/
+  mkdir -p /usr/local/etc/redis 
+  cp $root/6379.conf /usr/local/etc/redis/ 
+  chmod +x /etc/init.d/redis_6379
+  mkdir -p /logs/redis
+  chown -R www /logs
+  /etc/init.d/redis_6379 stop 
+  /etc/init.d/redis_6379 start
+}
+
+reload() {
+	#warning: data would be disappear!
+  /etc/init.d/redis_6379 stop 
+  /etc/init.d/redis_6379 start
+}
+
+main $1
