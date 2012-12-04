14bead8837e84889f05d6450758332ae14d80c19
Smart Lee <ismtlee@gmail.com>
2012-12-04 10:36:27 +0800

    .

--- pig/statics.sh
+++ pig/statics.sh
@@ -3,6 +3,6 @@ source /etc/profile
 root=`dirname $0`
 lastday=`date -d yesterday +%Y%m%d`
 logfile=/usr/local/nginx/logs/2012/access_$lastday
-output=$root/output_$lastday
+output=$root/output/download_ip_$lastday
 rm -rf $output
 pig -x local -param out_dir=$output -param logfile=$logfile $root/statics_by_nginx.pig 
