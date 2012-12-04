ff2d6ffff2ee20fe60d11cf321a515de75320c4a
Smart Lee <ismtlee@gmail.com>
2012-12-04 18:08:56 +0800

    .

--- pig/download_ip.sh
+++ pig/download_ip.sh
@@ -7,4 +7,6 @@ output=$root/output/download_ip_$lastday
 rm -rf $output
 pig -x local -param out_dir=$output -param logfile=$logfile $root/download_ip.pig 
 
+mail -s download_$lastday he.hq@joymeng.com < $output/part-r-00000
+echo "send mail..."
 find $root/output -type f -mtime +6 -exec rm {} \;
