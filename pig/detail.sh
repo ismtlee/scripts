8d1917d131f4d1abb6b67ab8a1ff08302017c4d9
Smart Lee <ismtlee@gmail.com>
2012-12-04 15:15:30 +0800

    .

--- pig/detail.sh
+++ pig/detail.sh
@@ -2,7 +2,7 @@
 source /etc/profile
 root=`dirname $0`
 lastday=`date -d yesterday +%Y%m%d`
-logfile=/usr/deploy/jmmq/logs/pluginlog_$lastday_*  
+logfile=/usr/deploy/jmmq/logs/pluginlog_${lastday}_*  
 output=$root/output/detail_$lastday
 rm -rf $output
 pig -x local -param out_dir=$output -param logfile=$logfile $root/detail.pig 
