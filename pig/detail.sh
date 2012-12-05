0eb67d67298ca8746db1c10e9f6ab8149087bc62
Smart Lee <ismtlee@gmail.com>
2012-12-05 15:15:47 +0800

    .

--- pig/detail.sh
+++ pig/detail.sh
@@ -7,6 +7,6 @@ output=$root/output/detail_$lastday
 rm -rf $output
 pig -x local -param out_dir=$output -param logfile=$logfile $root/detail.pig 
 
-mail -s report_$lastday he.hq@joymeng.com < $output/part-r-00000
+mail -s report_$lastday he.hq@joymeng.com -- -f li.jun@joymeng.com < $output/part-r-00000
 
 find $root/output -type f -mtime +6 -exec rm {} \;
