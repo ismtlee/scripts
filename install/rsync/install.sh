cce093315d915212505df8b68a107ec51c3eaee7
Smart Lee <ismtlee@gmail.com>
2012-12-17 17:53:25 +0800

    .

---
+++ install/rsync/install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+source ../header.sh
+version=3.0.9
+inner_ip=default
+
+dependencies() {
+	if [ x$2 = "x" ];then
+	 echo 'Pls specify the inner IP, ex, 192.168.1.10'
+	 exit
+  fi
+  inner_ip=$2	
+}
+
+download() {
+  tgz=rsync-$version.tar.gz
+
+	if [ ! -f $download/$tgz ];
+	then
+		wget ftp://ftp.samba.org/pub/rsync/$tgz 
+		tar zxvf $tgz 
+	fi
+}
+
+install() {
+	cd $download/rsync-$version
+	./configure --prefix=${prefix}/rsync 
+	make;make install
+}
+
+usergroup() {
+	echo 'no need'
+}
+
+config() {
+	mkdir /etc/rsyncd
+	cp $root/rsyncd /etc/init.d/
+  cp $root/rsyncd.conf /etc/rsyncd/
+	sed -i 's/inner_ip/$inner_ip/' /etc/rsyncd/rsyncd.conf 
+  cp $root/rsyncd.secrets /etc/rsyncd/
+  cp $root/rsyncd.motd /etc/rsyncd/
+  cp $root/rsync.password /etc/rsyncd/
+	chmod 600 /etc/rsyncd/rsyncd.secrets
+	chmod 600 /etc/rsyncd/rsync.password
+  chmod +x /etc/init.d/rsyncd
+  /etc/init.d/nginx restart
+}
+
+reload() {
+	/etc/init.d/nginx reload
+}
+
+main $1
