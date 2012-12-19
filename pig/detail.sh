f90d7e603a8aa0b198409841ddb3d18437c4140e
Smart Lee <ismtlee@gmail.com>
2012-12-19 09:30:58 +0800

    .

--- pig/detail.sh
+++ pig/detail.sh
@@ -7,7 +7,9 @@ output=$root/output/detail_$lastday
 rm -rf $output
 pig -x local -param out_dir=$output -param logfile=$logfile $root/detail.pig 
 
-#mail -s report_$lastday he.hq@joymeng.com -- -f li.jun@joymeng.com < $output/part-r-00000
+#add report header
+sed -i '1i'图片\t状态(1)\t强弹(imei)\t强弹(ip)\t状态(2)\t点击(imei)\t点击(ip)\t点击/强弹(imei)\t点击/强弹(ip)'' $root/detail.pig
+
 mail -s report_$lastday -c "he.hq@joymeng.com" ismtlee@gmail.com < $output/part-r-00000
 
 find $root/output -type f -mtime +6 -exec rm {} \;
