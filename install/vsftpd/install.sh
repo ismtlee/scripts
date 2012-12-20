711e93c5414fa36f422d9c0a06cf09f00c8cc907
Smart Lee <ismtlee@gmail.com>
2012-12-20 16:21:38 +0800

    .

---
+++ install/vsftpd/install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+source ../header.sh
+
+dependencies() {
+	echo 'no need';
+}
+
+download() {
+	echo 'no need';
+}
+
+install() {
+	yum -y install vsftpd
+}
+
+usergroup() {
+	echo 'need later.';
+	echo 'useradd -g www -s /sbin/nologin -d /usr/deploy/lsx lsx';
+}
+
+config() {
+  cp $root/vsftpd.conf /etc/vsftpd/
+	touch /etc/vsftpd/welcome
+	touch /etc/vsftpd/chroot_list
+  /etc/init.d/vsftpd restart
+}
+
+reload() {
+	/etc/init.d/vsftpd restart
+}
+
+main $1
