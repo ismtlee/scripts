ab8fcb5d2444ef87c0c472e61cf68bd6c35c6639
Smart Lee <ismtlee@gmail.com>
2012-12-21 13:34:39 +0800

    .

--- pig/country.sh
+++ pig/country.sh
@@ -7,6 +7,6 @@ output=$root/output/country_$lastday
 rm -rf $output
 pig -x local -param out_dir=$output -param logfile=$logfile $root/country.pig 
 
-mail -s report_$lastday -c "he.hq@joymeng.com" ismtlee@gmail.com < $output/part-r-00000
+mail -s country_$lastday -c "he.hq@joymeng.com" ismtlee@gmail.com < $output/part-r-00000
 
 find $root/output -type f -mtime +6 -exec rm {} \;
