e81355d3fd4dd1fa2492277ede3c1b7162c2cddb
Smart Lee <ismtlee@gmail.com>
2013-01-15 23:12:19 +0800

    .

---
+++ pig/operator.pig
@@ -0,0 +1,8 @@
+A = load '$logfile' using PigStorage('\t');
+--A = load '/usr/deploy/jmmq/logs/pluginlog_20121203_00' using PigStorage('\t');
+C = foreach A generate $8 as operator, $12 as imei; 
+D = distinct C;
+E =  group D by operator;
+F = foreach E generate group, COUNT(D.operator) as mycount;
+G = order F by mycount desc;
+STORE G INTO '$out_dir';
---
+++ pig/operator.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source /etc/profile
+root=`dirname $0`
+lastday=`date -d yesterday +%Y%m%d`
+#logfile=/usr/deploy/jmmq/logs/log2_${lastday}_*  
+logfile=/usr/deploy/jmmq/logs/log2_${lastday}_01
+output=$root/output/operator_$lastday
+rm -rf $output
+pig -x local -param out_dir=$output -param logfile=$logfile $root/operator.pig 
+
+#mail -s country_$lastday -c "he.hq@joymeng.com" ismtlee@gmail.com < $output/part-r-00000
+#mail -s country_$lastday  he.hq@joymeng.com < $output/part-r-00000
+
+find $root/output -type f -mtime +6 -exec rm {} \;
