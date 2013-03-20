75cbd017cb59effeadd9fa5e24bca8d6b3172e17
Smart Lee <ismtlee@gmail.com>
2013-03-20 15:20:40 +0800

    .

--- install/mysql/bakup.sh
+++ install/mysql/bakup.sh
@@ -7,7 +7,8 @@
 # To restore the db, for example:
 # gunzip < /data/bakup/mysql/all_2011_10_12_10_57_12.gz |mysql -p
 
-PATH=/usr/local/mysql/bin:/usr/bin:/bin
+#PATH=/usr/local/mysql/bin:/usr/bin:/bin
+PATH=/opt/mysql/bin:/usr/bin:/bin
 TO_DIR=/data/bakup/mysql/
 #dump
 mysqldump -pspvfLy --all-databases|gzip > ${TO_DIR}all_`date +%Y_%m_%d_%H_%M_%S`.gz
