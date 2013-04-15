0cf4b17cca8ae2cae33dca3671d4eb49e1b6144f
Smart Lee <ismtlee@gmail.com>
2013-04-15 18:07:32 +0800

    .

--- install/java/install.sh
+++ install/java/install.sh
@@ -20,7 +20,7 @@ fi
 wget http://download.oracle.com/otn-pub/java/jdk/7u17-b02/$pkgname --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" -O $pkgname
 mkdir /usr/java 
 rpm -ivh $pkgname  
-echo "export JAVA_HOME=/usr/java/jdk1.7.0_05/" >> /etc/profile
+echo "export JAVA_HOME=/usr/java/jdk1.7.0_17/" >> /etc/profile
 echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> /etc/profile
 source /etc/profile
 
