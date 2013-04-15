b42236a8bbcb1a88b686b62a2c9500f8b8fa104e
Smart Lee <ismtlee@gmail.com>
2013-04-15 18:04:11 +0800

    .

--- install/java/install.sh
+++ install/java/install.sh
@@ -17,8 +17,8 @@ fi
 #32 bit
 #wget http://download.oracle.com/otn-pub/java/jdk/7u17-b02/jdk-7u17-linux-i586.rpm --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com"
 #64 bit
-wget http://download.oracle.com/otn-pub/java/jdk/7u17-b02/$pkgname --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com"
-mkdir /usr/java -O $pkgname
+wget http://download.oracle.com/otn-pub/java/jdk/7u17-b02/$pkgname --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" -O $pkgname
+mkdir /usr/java 
 rpm -ivh $pkgname  
 echo "export JAVA_HOME=/usr/java/jdk1.7.0_05/" >> /etc/profile
 echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> /etc/profile
