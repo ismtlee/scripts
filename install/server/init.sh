0ed1b920859d223e1de7b721a91cde71d88816f8
Smart Lee <ismtlee@gmail.com>
2013-04-27 16:31:11 +0800

    .

--- install/server/init.sh
+++ install/server/init.sh
@@ -3,8 +3,16 @@
 #update first
 yum update -y
 
+bit=`getconf LONG_BIT`
+
 #file descriptor limits
 echo "* soft nofile 655360" >> /etc/security/limits.conf  
 echo "* hard nofile 655360" >> /etc/security/limits.conf  
-echo "session required pam_limits.so" >> /etc/pam.d/login 
+
+if [ $bit == 32 ]; then
+	echo "session required /lib/security/pam_limits.so" >> /etc/pam.d/login 
+else
+	echo "session required /lib64/security/pam_limits.so" >> /etc/pam.d/login 
+fi
+
 
