d3533e448c2083705d8e55e923184e2051ce0397
Smart Lee <ismtlee@gmail.com>
2013-06-05 16:16:11 +0800

    .

--- statics/thefunnytime/reg.sh
+++ statics/thefunnytime/reg.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 dat=$(date -d y“yesterday” +%Y%m%d)
+src_log=access_funny_api_$dat.log
+src_path=/logs/nginx/$(date -d y“yesterday” +%Y)/
+dist_path=/logs/thefunnytime/$(date -d y“yesterday” +%Y)/
 
-echo $dat
+#new user
+grep -o "guid=\w*&uid=0" ${src_path}${src_log} |grep -o "guid=\w*"|awk -F"=" '{print $2}'|sort -u > $dist_path/reg_$dat.log
+#active user
+grep -o "guid=\w*&uid" ${src_path}${src_log} |grep -o "guid=\w*"|awk -F"=" '{print $2}'|sort -u > $dist_path/active_$dat.log
+#open num
+grep -o "guid=\w*&uid" ${src_path}${src_log} |wc -l > $dist_path/open_$dat.log
+
+
