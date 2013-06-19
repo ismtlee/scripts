0cc934563ff16b7df09842ae1783833d05ea4909
Smart Lee <ismtlee@gmail.com>
2013-06-19 19:48:01 +0800

    .

--- statics/thefunnytime/retension.sh
+++ statics/thefunnytime/retension.sh
@@ -1,19 +1,48 @@
-#!/bin/sh
+#!/bin/bash
+#Pls use bash command, not sh
+
 DAYS=7
 BASE_DATE=`date -d '-'$DAYS'day' +%Y%m%d`
 TOP_DATE=`date -d '-1 day' +%Y%m%d`
 LOG_PATH=/logs/thefunnytime.bak/
 
-for i in $(seq $DAYS -1 1)
+iHeader() {
+	info="统计日期\t"
+  for i in $(seq 1 1 $1)
+	do
+		info=$info$i日留存"\t"
+	done
+	echo -e $info
+}
 
+iHeader $DAYS
+
+for i in $(seq $DAYS -1 1)
 do 
   REG=`date -d '-'$i'day' +%Y%m%d`
-  b=`sort -u reg_$REG.log|wc -l`
-  ACTIVE=1; 
-  while [ $REG -lt $BASE_DATE ] 
-  do
-    a=`join <(cat $LOG_PATH/reg_$REG.log|sort -u) <(cat $LOG_PATH/active_$ACTIVE.log|sort -u )|wc -l`
-    $ACTIVE
-  done  
-
+  REG_LOGS[$i]=$LOG_PATH'reg_'$REG.log
+  ACTIVE_LOGS[$i]=$LOG_PATH'active_'$REG.log
 done
+
+for ary in ${REG_LOGS[@]}
+do 
+	b=`sort -u $ary |wc -l`
+	b_date=${ary##*_}
+	b_date=${b_date:0:8}
+	text=${b_date}"\t"
+
+	for (( idx=${#ACTIVE_LOGS[@]}; idx>=1 ; idx-- )) ; do
+     ary1=${ACTIVE_LOGS[idx]}
+     a_date=${ary1##*_}
+     a_date=${a_date:0:8}
+     if [ $b_date -lt $a_date ]; then
+	     a=`join <(cat $ary|sort -u) <(cat $ary1|sort -u)|wc -l`
+       text=$text`echo "scale=2;$a*100/$b"|bc`%"\t"
+     fi
+	done
+
+	echo -e $text
+done
+
+
+
