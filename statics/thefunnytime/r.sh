bb150d225230cf5c6ed4fff06be85877ede183a9
Smart Lee <ismtlee@gmail.com>
2013-06-19 15:52:24 +0800

    .

--- statics/thefunnytime/r.sh
+++ statics/thefunnytime/r.sh
@@ -18,8 +18,10 @@ do
 	b=`sort -u $ary |wc -l`
   for ary1 in ${ACTIVE_LOGS[@]}
 	do
+		if [ $ary < $ary1 ]; then
 		a=`join <(cat $ary|sort -u) <(cat $ary1|sort -u)|wc -l`
 		echo `echo "scale=2;$a*100/$b"|bc`% 
+	  fi
 	done	
 done
 
