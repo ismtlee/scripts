470879a22530525c73baaabb1467a28206edec21
Smart Lee <ismtlee@gmail.com>
2013-06-19 14:25:08 +0800

    .

--- statics/thefunnytime/r.sh
+++ statics/thefunnytime/r.sh
@@ -4,7 +4,7 @@ BASE_DATE=`date -d '-'$DAYS'day' +%Y%m%d`
 TOP_DATE=`date -d '-1 day' +%Y%m%d`
 LOG_PATH=/logs/thefunnytime.bak/
 
-for i in $(seq 1 1 $DAYS)
+for i in $(seq $DAYS -1 1)
 do 
   REG=`date -d '-'$i'day' +%Y%m%d`
   REG_LOGS[$i]=$LOG_PATH'REG_'$REG.log
@@ -13,11 +13,11 @@ done
 
 for ary in ${REG_LOGS[@]}
 do 
+	b=`sort -u $ary |wc -l`
   for ary1 in ${ACTIVE_LOGS[@]}
 	do
-	  echo "a"	
+    a=`join <(cat $ary|sort -u) <(cat $ary1|sort -u )|wc -l`
+		echo `echo "scale=2;$a*100/$b"|bc`%
 	done	
-
-	echo $ary
 done
 
