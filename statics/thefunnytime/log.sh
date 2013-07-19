bd33ae8bf7e50fa22e31af1dfa9e494effecce52
Smart Lee <ismtlee@gmail.com>
2013-07-19 15:23:51 +0800

    .

--- statics/thefunnytime/log.sh
+++ statics/thefunnytime/log.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #generate logs
 
-dat=$(date -d y“yesterday” +%Y%m%d)
+#dat=$(date -d y“yesterday” +%Y%m%d)
+dat=`date -d '-1 day' +%Y%m%d`
 src_log=access_funny_api_$dat.log
 src_path=/logs/nginx/$(date -d y“yesterday” +%Y)/
 dist_path=/logs/thefunnytime/$(date -d y“yesterday” +%Y)/
