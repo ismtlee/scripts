9c08021bc86036af97bc720cd4d7f7352ebd46cb
Smart Lee <ismtlee@gmail.com>
2013-08-07 15:52:44 +0800

    .

--- install/php/install_extension.sh
+++ install/php/install_extension.sh
@@ -3,6 +3,14 @@ YAF_V=2.2.9
 CMD_PHPIZE=/usr/local/cellar/php54/bin/phpize
 cd ~/Downloads
 
+yaf() {
+  wget http://pecl.php.net/get/yaf-$YAF_V.tgz
+	tar zxvf yaf-$YAF_V.tgz
+	cd yaf-$YAF_V
+	$CMD_PHPIZE 
+	./configure;make;make install
+}
+
 case $1 in
    yaf)
 		 yaf
@@ -24,10 +32,4 @@ case $1 in
 esac
 	
 
-yaf() {
-  wget http://pecl.php.net/get/yaf-$YAF_V.tgz
-	tar zxvf yaf-$YAF_V.tgz
-	cd yaf-$YAF_V
-	$CMD_PHPIZE 
-	./configure;make;make install
-}
+
