e7fe8b8313a0b2c9f79853de18182c749e54b7e3
Smart Lee <ismtlee@gmail.com>
2013-09-06 10:46:54 +0800

    .

---
+++ install/inotify-tools/sample.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#-- sh sample.sh& -- 
+host=204.45.0.66
+
+src=/usr/deploy/statics/adpic/
+dst=adpic
+
+
+sync() {
+        rsync -avz  --delete  --force --exclude=".svn/***" --password-file=/etc/rsyncd/rsync.password  $src www@$1::$dst
+}
+
+/usr/local/bin/inotifywait -mrq  --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e modify,delete,create,attrib $src | while read files;
+do
+ sync $host
+done
