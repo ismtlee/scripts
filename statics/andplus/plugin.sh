025b24ac17df3a585eee00866e49ffac653e084a
Smart Lee <ismtlee@gmail.com>
2013-09-16 16:06:37 +0800

    .

--- statics/andplus/plugin.sh
+++ statics/andplus/plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-src=test
-dst=test1
+src=/logs/test
+dst=/logs/test1
 grep a=request $src |grep operator|awk '{print $1, $2, $7}'|awk -F '&' '{print $1, $2, $3, $4, $5}'> $dst
 sed -i 's/\w*= /_ /g' $dst
 sed -i 's/\w*=//g' $dst
