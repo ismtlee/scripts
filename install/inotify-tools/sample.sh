b4e5ce9929258e10d852aa4b23207b3fb2dffc60
Smart Lee <ismtlee@gmail.com>
2013-09-26 22:50:39 +0800

    .

--- install/inotify-tools/sample.sh
+++ install/inotify-tools/sample.sh
@@ -11,7 +11,7 @@ sync() {
         rsync -avz  --delete  --force --exclude=".svn/***" --password-file=/etc/rsyncd/rsync.password  $src www@$1::$dst
 }
 
-/usr/local/cellar/inotify/bin/inotifywait -mrq  --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e modify,delete,create,attrib $src | while read files;
+/usr/local/cellar/inotify/bin/inotifywait --exclude=".svn/***" -mrq  --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e modify,delete,create,attrib $src | while read files;
 do
  sync $host
 done
