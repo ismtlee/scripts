e8b63af955e52d7d3166c728e243c9f75cadfe7d
Smart Lee <ismtlee@gmail.com>
2013-11-17 23:08:37 +0800

    .

--- statics/andplus/packname_imei.sh
+++ statics/andplus/packname_imei.sh
@@ -2,8 +2,8 @@
 source /etc/profile
 root=`dirname $0`
 logfile=$dst
-output=$log_path/output/packname_$LASTDAY
+output=$log_path/output/packname_imei_$LASTDAY
 rm -rf $output
-pig -x local -param out_dir=$output -param logfile=$logfile $root/packname.pig
+pig -x local -param out_dir=$output -param logfile=$logfile $root/packname_imei.pig
 #mail -s country_$lastday -c "he.hq@joymeng.com" ismtlee@gmail.com < $output/part-r-00000
 
