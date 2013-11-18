4dc8af5e9777960d088c4591364baa0ce03cd1ac
Smart Lee <ismtlee@gmail.com>
2013-11-18 15:00:20 +0800

    .

--- pig/users.sh
+++ pig/users.sh
@@ -6,6 +6,7 @@ logfile=/usr/deploy/jmmq/logs/log2_${lastday}_*
 output=$root/output/users_$lastday
 rm -rf $output
 pig -x local -param out_dir=$output -param logfile=$logfile $root/users.pig 
+
 #mail -s country_$lastday -c "he.hq@joymeng.com" ismtlee@gmail.com < $output/part-r-00000
 #mail -s country_$lastday  he.hq@joymeng.com < $output/part-r-00000
 
