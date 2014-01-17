19dd7be11a62d3320e207b0a226b195850f24e22
Smart Lee <ismtlee@gmail.com>
2014-01-17 10:19:09 +0800

    .

--- statics/andplus/letang_box.sh
+++ statics/andplus/letang_box.sh
@@ -13,18 +13,18 @@ vvmore=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click
 vvfigure=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_figure"|wc -l`
 vvstart=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "game_launch"|wc -l`
 #排重
-#v0=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_window&t=0"|wc -l`
-#v10=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_window&t=10"|wc -l`
-#v60=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_window&t=60"|wc -l`
-#v110=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_window&t=110"|wc -l`
-#v300=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_window&t=300"|wc -l`
-#vobtain=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_obtain"|wc -l`
-#vmore=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_more"|wc -l`
-#vfigure=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_figure"|wc -l`
-#vstart=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "game_launch"|wc -l`
+v0=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_window&t=0"|grep -oE "imei=(\w|-)*"|sort -u|wc -l`
+v10=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_window&t=10"|grep -oE "imei=(\w|-)*"|sort -u|wc -l`
+v60=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_window&t=60"|grep -oE "imei=(\w|-)*"|sort -u|wc -l`
+v110=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_window&t=110"|grep -oE "imei=(\w|-)*"|sort -u|wc -l`
+v300=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_window&t=300"|grep -oE "imei=(\w|-)*"|sort -u|wc -l`
+vobtain=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_obtain"|grep -oE "imei=(\w|-)*"|sort -u|wc -l`
+vmore=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_more"|grep -oE "imei=(\w|-)*"|sort -u|wc -l`
+vfigure=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "click_figure"|grep -oE "imei=(\w|-)*"|sort -u|wc -l`
+vstart=`grep -v 218.104.71.178 $LOG_PATH/promote_static_$LASTDAY.log|grep "game_launch"|grep -oE "imei=(\w|-)*"|sort -u|wc -l`
 
 echo -e "-----------------------------不排重统计--------------------------------->>>>>"
-echo -e "时间未到\t"$vv0 >> $LOG_PATH/output/promote_$LASTDAY
+echo -e "时间未到\t"$vv0 > $LOG_PATH/output/promote_$LASTDAY
 echo -e "10s已到\t"$vv10 >> $LOG_PATH/output/promote_$LASTDAY
 echo -e "60s已到\t"$vv60 >> $LOG_PATH/output/promote_$LASTDAY
 echo -e "110s已到\t"$vv110 >> $LOG_PATH/output/promote_$LASTDAY
@@ -34,4 +34,13 @@ echo -e "点击moregame\t"$vvmore >> $LOG_PATH/output/promote_$LASTDAY
 echo -e "点击大图跳至gp\t"$vvfigure >> $LOG_PATH/output/promote_$LASTDAY
 echo -e "启动游戏\t"$vvstart >> $LOG_PATH/output/promote_$LASTDAY
 echo -e "-----------------------------排重统计----------------------------------->>>>>"
+echo -e "时间未到\t"$v0 >> $LOG_PATH/output/promote_$LASTDAY
+echo -e "10s已到\t"$v10 >> $LOG_PATH/output/promote_$LASTDAY
+echo -e "60s已到\t"$v60 >> $LOG_PATH/output/promote_$LASTDAY
+echo -e "110s已到\t"$v110 >> $LOG_PATH/output/promote_$LASTDAY
+echo -e "其他时间已到\t"$v300 >> $LOG_PATH/output/promote_$LASTDAY
+echo -e "点击获取金币\t"$vobtain >> $LOG_PATH/output/promote_$LASTDAY
+echo -e "点击moregame\t"$vvmore >> $LOG_PATH/output/promote_$LASTDAY
+echo -e "点击大图跳至gp\t"$vfigure >> $LOG_PATH/output/promote_$LASTDAY
+echo -e "启动游戏\t"$vstart >> $LOG_PATH/output/promote_$LASTDAY
 
