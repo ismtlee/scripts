f58e1c205e4f5cf94617c365c697bf824c06e483
Smart Lee <ismtlee@gmail.com>
2014-02-24 09:20:47 +0800

    .

--- statics/andplus/android.sh
+++ statics/andplus/android.sh
@@ -5,5 +5,5 @@ logfile=$dst
 output=$log_path/output/android_$LASTDAY
 rm -rf $output
 pig -x local -param out_dir=$output -param logfile=$logfile $root/android.pig
-#mail -s country_$lastday -c "he.hq@joymeng.com" ismtlee@gmail.com < $output/part-r-00000
+#mail -s country_$lastday -c "he.hq@gmail.com" ismtlee@gmail.com < $output/part-r-00000
 
--- statics/andplus/operator.sh
+++ statics/andplus/operator.sh
@@ -5,5 +5,5 @@ logfile=$dst
 output=$log_path/output/operator_$LASTDAY
 rm -rf $output
 pig -x local -param out_dir=$output -param logfile=$logfile $root/operator.pig
-#mail -s country_$lastday -c "he.hq@joymeng.com" ismtlee@gmail.com < $output/part-r-00000
+#mail -s country_$lastday -c "he.hq@gmail.com" ismtlee@gmail.com < $output/part-r-00000
 
