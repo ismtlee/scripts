580e1919655dd572e56be68631900f9d0a9a40b3
Smart Lee <ismtlee@gmail.com>
2014-02-25 09:40:30 +0800

    .

--- statics/andplus/users2.sh
+++ statics/andplus/users2.sh
@@ -1,2 +1,4 @@
-grep -oE "uuid=(\w|-)*" /logs/nginx/2013/rsyncd/access_andplus_all_20130928.log|sort -u|wc -l
+YEAR=`date -d '-1 day' +%Y`
+LASTDAY=`date -d '-1 day' +%Y%m%d`
+grep -oE "uuid=(\w|-)*" /logs/nginx/${YEAR}/rsyncd/access_andplus_all_${LASTDAY}.log|sort -u|wc -l
 
