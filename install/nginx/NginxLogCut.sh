5111a136a2c754377f2e51f18877b5246d5ceddb
Smart Lee <ismtlee@gmail.com>
2014-04-14 09:44:13 +0800

    .

--- install/nginx/NginxLogCut.sh
+++ install/nginx/NginxLogCut.sh
@@ -3,14 +3,15 @@
 #00 00 * * * /bin/bash  /usr/local/nginx/sbin/NginxLogCut.sh
 
 logs_path=/logs/nginx/
-save_path=${logs_path}$(date -d y“yesterday” +%Y)/
+LASTDAY=`date -d '-1 day' +%Y%m%d`
+save_path=${logs_path}$LASTDAY/
 
 
 mkdir -p ${save_path}
 
 
-mv ${logs_path}error.log ${save_path}/error_$(date -d y“yesterday” +%Y%m%d).log
-mv ${logs_path}access.log ${save_path}/access_$(date -d y“yesterday” +%Y%m%d).log
+mv ${logs_path}error.log ${save_path}/error_$LASTDAY.log
+mv ${logs_path}access.log ${save_path}/access_$LASTDAY.log
 
 /etc/init.d/nginx reload
 
