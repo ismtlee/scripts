d600114c2cf88a2542c578b2c3d1d3dc9e620218
Smart Lee <ismtlee@gmail.com>
2014-06-13 17:44:54 +0800

    .

--- statics/andplus/promotion/1.php
+++ statics/andplus/promotion/1.php
@@ -20,7 +20,8 @@ foreach($a as $e) {
     $c = array('carrier' => $b[5], 
       'promotion' => $b[6],
       'total' => $b[3],
-      'func' => $b[4]);
+      'func' => $b[4],
+      'date' => $date);
     $d[] = $c;
   }
 }
@@ -32,4 +33,79 @@ foreach($d as &$e) {
   $e['uuid'] = `grep ${e['carrier']} $file |grep ${e['promotion']} |grep ${e['func']}|awk '{print $6}'|sort -u|wc -l`; 
 }
 
-print_r($d);
+write2Excel($d, $date);
+
+
+function writeHead($objActSheet) {
+ $ledger_tabname = array(
+     'date' => '日期',
+     'func' =>'功能',
+     'carrier' => '载体应用',
+     'promotion' => '推广应用',
+     'ip' => 'ip排重',
+     'uuid' => 'uuid排重',
+     'imei' => 'imei排重',
+     'imsi' => 'imsi排重',
+     'total' => '总请求数',
+  );
+  $tab_color = 'B2A1C7';
+  $CHAR_ADD = 'A';
+  foreach ($ledger_tabname as $e) {
+    writeCell($objActSheet, $e, $CHAR_ADD++, 1, $tab_color);
+  }
+}
+
+function writeData($objActSheet, $list) {
+  $i = 2;
+  $color = 'CCCCCC';
+  foreach($list as $e) {
+    $CHAR_ADD = 'A';
+    writeCell($objActSheet, $e['date'], $CHAR_ADD++, $i, $color);
+    writeCell($objActSheet, $e['func'], $CHAR_ADD++, $i, $color);
+    writeCell($objActSheet, $e['carrier'], $CHAR_ADD++, $i, $color);
+    writeCell($objActSheet, $e['promotion'], $CHAR_ADD++, $i, $color);
+    writeCell($objActSheet, $e['ip'], $CHAR_ADD++, $i, $color);
+    writeCell($objActSheet, $e['uuid'], $CHAR_ADD++, $i, $color);
+    writeCell($objActSheet, $e['imei'], $CHAR_ADD++, $i, $color);
+    writeCell($objActSheet, $e['imsi'], $CHAR_ADD++, $i, $color);
+    writeCell($objActSheet, $e['total'], $CHAR_ADD++, $i, $color);
+    $objActSheet->getColumnDimension($CHAR_ADD)->setAutoSize(true);
+    ++$i;
+  }
+}
+
+function write2Excel($list, $curtime) {
+  $objPHPExcel = new PHPExcel();
+  $objActSheet = initExcel($objPHPExcel);
+  $CHAR_ADD = 'A';
+  $title = "promotion";
+  writeHead($objActSheet);
+  writeData($objActSheet, $list);
+
+  header('Content-Type: application/vnd.ms-excel; charset=UTF-8');
+  //文件名
+  $ua = $_SERVER["HTTP_USER_AGENT"];
+  $filename = "${title}_$curtime.xls";
+  $encoded_filename = urlencode($filename);
+  $encoded_filename = str_replace("+", "%20", $encoded_filename);
+
+  if (preg_match("/MSIE/", $ua)) {
+    header('Content-Disposition: attachment; filename="' . $encoded_filename . '"');
+  } else if (preg_match("/Firefox/", $ua)) {
+    header('Content-Disposition: attachment; filename*="utf8\'\'' . $filename . '"');
+  } else {
+    header('Content-Disposition: attachment; filename="' . $filename . '"');
+  }
+
+  header('Cache-Control: max-age=0');
+  $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+  $objWriter->save('php://output');
+}
+
+function writeCell($objActSheet, $value, $x, $y, $back_color) {
+  $objActSheet->setCellValue($x.$y, $value);
+  $objActSheet->getStyle($x.$y)->applyFromArray(
+     array('fill' => array(
+        'type' => PHPExcel_Style_Fill::FILL_SOLID,
+        'color' => array('rgb' => $back_color))));
+}
