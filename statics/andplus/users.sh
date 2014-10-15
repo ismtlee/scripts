64bf98d3917586d8ba79addc30cd6497a2791e78
Smart Lee <ismtlee@gmail.com>
2014-10-15 10:40:46 +0800

    .

--- statics/andplus/users.sh
+++ statics/andplus/users.sh
@@ -6,4 +6,4 @@ output=$log_path/output/users_$LASTDAY
 rm -rf $output
 pig -x local -param out_dir=$output -param logfile=$logfile $root/users.pig
 #mail -s country_$lastday -c "he.hq@gmail.com" ismtlee@gmail.com < $output/part-r-00000
-
+mail -s PEKING_DAU_$LASTDAY -c "18263520@qq.com" ismtlee@gmail.com < $output/part-r-00000
