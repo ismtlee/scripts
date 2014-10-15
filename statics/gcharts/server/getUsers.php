9b60c28a7ec2c7a4748213f40ef8f1316a0bbf95
Smart Lee <ismtlee@gmail.com>
2014-10-15 16:51:40 +0800

    .

---
+++ statics/gcharts/server/getUsers.php
@@ -0,0 +1,10 @@
+<?php 
+
+// This is just an example of reading server side data and sending it to the client.
+// // It reads a json formatted text file and outputs it.
+//
+
+$d=strtotime('-3 days');
+echo $d;
+
+?>
