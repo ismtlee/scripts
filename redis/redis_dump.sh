ed8458b85cb0f15e05a68e125ca5fe731bdbfe42
Smart Lee <ismtlee@gmail.com>
2016-08-12 17:39:28 +0800

    .

--- redis/redis_dump.sh
+++ redis/redis_dump.sh
@@ -2,12 +2,14 @@
 # example:
 # sh redis_dump.sh MUS data 6380
 PREFIX=$1
-DUMP_FILE=$2
-PORT=$3
+DUMP_DIR=$2
+PORT=6380
 keys=`redis-cli -p $PORT KEYS ${PREFIX}_*`
-rm $DUMP_FILE
+#rm -rf $DUMP_DIR
+mkdir -p $DUMP_DIR
 for k in $keys
 do
-  line=$k"\t"`redis-cli -p $PORT --raw dump $k|head -c-1`
-  echo -e $line >> $DUMP_FILE
+  redis-cli -p 6380 --raw dump $k|head -c-1 >> $DUMP_DIR/$k
 done
+tar zcvf $DUMP_DIR.tgz $DUMP_DIR
+rm -rf $DUMP_DIR
--- redis/redis_restore.sh
+++ redis/redis_restore.sh
@@ -2,6 +2,12 @@
 # example
 # sh redis_restore.sh data 6379
 #filename, no extension. say data.tgz mean data.
-DUMP_FILE=$1 
-PORT=$2
-awk '{script="redis-cli -p '$PORT'  restore "$1" 0 "$2;system(script) }' $DUMP_FILE
+DUMP_FILE=$1
+PORT=6379
+tar zxvf $DUMP_FILE.tgz
+list=`ls $DUMP_FILE`
+for k in $list 
+do 
+  cat $DUMP_FILE/$k|redis-cli -p $PORT -x restore $k 0
+done
+
