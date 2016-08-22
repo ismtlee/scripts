3ecc56f4f9ecf36d6010cf2cae025241c45785d9
Smart Lee <ismtlee@gmail.com>
2016-08-22 16:23:12 +0800

    .

--- install/mysql/bakup.sh
+++ install/mysql/bakup.sh
@@ -6,6 +6,8 @@
 # @gmail: ismtlee@gmail.com
 # To restore the db, for example:
 # gunzip < /data/bakup/mysql/all_2011_10_12_10_57_12.gz |mysql -p
+# if u wanna restore one single table data from a big huge dump file, try
+# gunzip < all_20160821_210001.gz |sed -n -e '/CREATE TABLE.*`rp_accounts`/,/UNLOCK TABLES/p' > 1.dump, rp_accounts's a table name.
 
 #PATH=/usr/local/mysql/bin:/usr/bin:/bin
 PATH=/opt/mysql/bin:/usr/bin:/bin
