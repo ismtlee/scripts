4e06f41ec5877dbda5421699e4eff34eafcd92ad
Smart Lee <ismtlee@ivymobile.com>
2017-06-01 17:13:20 +0800

    .

--- install/nginx/NginxLogCut.sh
+++ install/nginx/NginxLogCut.sh
@@ -14,7 +14,8 @@ mkdir -p ${save_path}
 mv ${logs_path}error.log ${save_path}/error_$LASTDAY.log
 mv ${logs_path}access.log ${save_path}/access_$LASTDAY.log
 
-/etc/init.d/nginx reload
+#/etc/init.d/nginx reload
+systemctl reload nginx
 
 #remove 
 find ${save_path} -type f -mtime +6 -exec rm {} \;
