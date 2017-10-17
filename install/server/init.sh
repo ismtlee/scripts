e511111e2c0f57147185b8f9fd739ea35a081e6a
Smart Lee <ismtlee@ivymobile.com>
2017-10-17 17:16:42 +0800

    .

--- install/server/init.sh
+++ install/server/init.sh
@@ -15,6 +15,15 @@ else
 	echo "session required /lib64/security/pam_limits.so" >> /etc/pam.d/login 
 fi
 
+cp  disable-transparent-hugepages /etc/init.d/
 
+chmod 755 /etc/init.d/disable-transparent-hugepages
 
+chkconfig --add disable-transparent-hugepages
 
+mkdir /etc/tuned/no-thp
+cp tuned.conf /etc/tuned/no-thp/
+tuned-adm profile no-thp
+#never is right or try to reboot the machine 
+#cat /sys/kernel/mm/transparent_hugepage/enabled
+#cat /sys/kernel/mm/transparent_hugepage/defrag
