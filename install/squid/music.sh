874411fe2af08f34bc3b298b638800d173ee4e96
Smart Lee <ismtlee@gmail.com>
2018-04-03 16:19:12 +0800

    .

--- install/squid/music.sh
+++ install/squid/music.sh
@@ -2,6 +2,7 @@
 #先安装node/centos7.sh squid/install.sh
 # sh music.sh
 mkdir /usr/deploy
+cd /usr/deploy/
 git clone https://github.com/Binaryify/NeteaseCloudMusicApi.git --depth 1
 cd NeteaseCloudMusicApi
 npm install apicache
