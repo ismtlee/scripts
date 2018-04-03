86820268023029cc34a3059a2d93ab660d4f2b25
Smart Lee <ismtlee@gmail.com>
2018-04-03 15:36:48 +0800

    .

---
+++ install/squid/music.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#先安装node/centos7.sh squid/install.sh
+# sh music.sh
+yum install git -y
+mkdir /usr/deploy
+git clone https://github.com/Binaryify/NeteaseCloudMusicApi.git --depth 1
+cd NeteaseCloudMusicApi
+npm install apicache
+npm install express
+npm install big-integer
+npm install request
+
+forever start app.js
