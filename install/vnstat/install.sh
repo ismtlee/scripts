3640f3e534895397e3e5378d1df43e9444775dfe
Smart Lee <ismtlee@gmail.com>
2018-05-30 16:40:26 +0800

    .

---
+++ install/vnstat/install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+source ../header.sh
+version=1.18
+
+dependencies() {
+  echo "no dependencies.." 
+}
+
+download() {
+  tgz=vnstat-$version.tar.gz
+
+	if [ ! -f $download/$tgz ];
+	then
+    wget http://humdi.net/vnstat/$tgz
+		tar zxvf $tgz 
+	fi
+}
+
+install() {
+	cd $download/vnstat-$version
+	#./configure --prefix=${prefix}/rsync 
+	./configure 
+	make;make install
+}
+
+usergroup() {
+	echo 'no need'
+}
+
+config() {
+  wget http://humdi.net/vnstat/init.d/redhat/vnstat -P /etc/init.d/
+  chmod +x /etc/init.d/vnstat
+  /etc/init.d/vnstat restart
+  chkconfig vnstat on
+}
+
+reload() {
+	/etc/init.d/vnstat restart
+}
+
+
+main $1
