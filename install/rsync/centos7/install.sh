206f9b7950ca5dcf0b6f27211195ee367ddef473
Smart Lee <ismtlee@gmail.com>
2020-04-14 20:53:57 +0800

    .

---
+++ install/rsync/centos7/install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+source ../header.sh
+version=3.1.2
+inner_ip=default
+
+dependencies() {
+  echo 'no need'
+}
+
+download() {
+  echo 'no need'
+}
+
+install() {
+  yum install -y rsync xinetd 
+}
+
+usergroup() {
+	echo 'no need'
+}
+
+config() {
+	mkdir /etc/rsyncd
+	cp $root/rsyncd /etc/init.d/
+  cp $root/rsyncd.conf /etc/rsyncd/
+	sed -i ''s/inner_ip/"$inner_ip"/'' /etc/rsyncd/rsyncd.conf 
+  cp $root/rsyncd.secrets /etc/rsyncd/
+  cp $root/rsyncd.motd /etc/rsyncd/
+  cp $root/rsync.password /etc/rsyncd/
+  cp $root/excludes /etc/rsyncd/
+	chmod 600 /etc/rsyncd/rsyncd.secrets
+	chmod 600 /etc/rsyncd/rsync.password
+  chmod +x /etc/init.d/rsyncd
+  /etc/init.d/rsyncd restart
+  chkconfig rsyncd on
+}
+
+reload() {
+	/etc/init.d/rsyncd restart
+}
+
+inner_ip=$2	
+ 
+if [ x$1 = "xinstall" ];then
+  if [ x$2 = "x" ];then
+    echo 'Pls specify the inner IP, ex, 192.168.1.10. Note, Address is not neccserry, however.'
+    exit
+  fi
+fi
+inner_ip=$2	
+
+main $1
