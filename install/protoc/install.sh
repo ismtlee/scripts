87e2afacb0bbfe4cc5fa03120449934ab600e573
Smart Lee <ismtlee@gmail.com>
2020-04-21 11:29:58 +0800

    .

--- install/protoc/install.sh
+++ install/protoc/install.sh
@@ -1,6 +1,6 @@
 #https://github.com/protocolbuffers/protobuf/releases
 PROTOC_ZIP=protoc-3.11.4-linux-x86_64.zip
-curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/$PROTOC_ZIP
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/$PROTOC_ZIP
 sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
 rm -f $PROTOC_ZIP
